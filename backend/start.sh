2851e370f7b4e8f75778c3f55ffe9f90c41df77b
Abhishek Mahajan <abhishek.mahajan314@gmail.com>
2026-05-21 10:02:22 -0400

    further nickpacks updates

--- backend/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-# apt-installed libs (libuuid1, libasound2t64, libssl) live here; Nix LD_PATH omits /usr/lib.
-export LD_LIBRARY_PATH="/usr/lib/x86_64-linux-gnu:/lib/x86_64-linux-gnu${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
-export PYTHONPATH="${PYTHONPATH:-.}"
-exec /opt/venv/bin/uvicorn main:app --host 0.0.0.0 --port "${PORT:?PORT not set}"
